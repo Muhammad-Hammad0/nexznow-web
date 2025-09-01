90e52cf961338cafe4dc05e71730a150e7e99422
Hammaddeveloper <hammaddeveloper02@gmail.com>
2025-09-01 15:15:55 +0500

    url change

--- frontend/my-app/src/contaxt/AuthContext.jsx
+++ frontend/my-app/src/contaxt/AuthContext.jsx
@@ -6,7 +6,7 @@ export const authDataContext = createContext();
 // Provider component
 export function AuthProvider({ children }) {
   // ✅ backend ka URL daalo, frontend ka nahi
-  const [serverUrl] = useState("https://nexzen-backend-production-3f38.up.railway.app");
+  const [serverUrl] = useState("https://nexznow-web.vercel.app");
 
   return (
     <authDataContext.Provider value={{ serverUrl }}>
