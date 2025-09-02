a05875c8c6df2e00fce4f04e93558eaaee3838c0
Hammaddeveloper <hammaddeveloper02@gmail.com>
2025-09-02 23:15:14 +0500

    n

--- frontend/my-app/src/contaxt/AuthContext.jsx
+++ frontend/my-app/src/contaxt/AuthContext.jsx
@@ -7,7 +7,7 @@ export const authDataContext = createContext();
 // Provider component
 export function AuthProvider({ children }) {
   // ✅ Correct backend URL
-  const [serverUrl] = useState("https://nexznow-web.vercel.app");
+  const [serverUrl] = useState("https://nexzen-backend-production-3f38.up.railway.app");
 
   // ✅ Setup axios instance (withCredentials ensures cookies like token are sent)
   const api = axios.create({
