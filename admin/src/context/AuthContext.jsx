bd6de42d0f5bf895d9de442d0d531553ca64e9c2
Hammaddeveloper <hammaddeveloper02@gmail.com>
2025-09-03 17:44:11 +0500

    new commit

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react';
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  let serverUrl = "main-backend-production-945c.up.railway.app"; // No slash at the end
+  let serverUrl = "https://main-backend-production-945c.up.railway.app"; // No slash at the end
 
   let value = {
     serverUrl
