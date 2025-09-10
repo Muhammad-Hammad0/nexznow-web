eec0b2a2c8fd764600cafca95da853fddf36730d
Hammaddeveloper <hammaddeveloper02@gmail.com>
2025-09-10 22:19:51 +0500

    admin url

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react';
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  let serverUrl = "https://main-backend-production-945c.up.railway.app"; // No slash at the end
+  let serverUrl = "http://srv1000765.hstgr.cloud/api"; // No slash at the end
 
   let value = {
     serverUrl
